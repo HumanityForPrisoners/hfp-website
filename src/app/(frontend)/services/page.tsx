bb8c21b07bb109fe86d8cde0dacf8a8eda9cc38c
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-03-26 23:26:21 -0400

    Change metadata

--- src/app/(frontend)/services/page.tsx
+++ src/app/(frontend)/services/page.tsx
@@ -76,6 +76,6 @@ export default async function Page() {
 
 export function generateMetadata(): Metadata {
   return {
-    title: `Payload Website Template Posts`,
+    title: `Humanity For Prisoners Posts`,
   }
 }
--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,15 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description:
+    "Ensuring that all of Michigan's prisoners are treated with compassion and dignity. Providing support to friends, families and others impacted by the MDOC. Humanity for Prisoners.",
   images: [
     {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
+      url: `${getServerSideURL()}/website-template.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Humanity For Prisoners',
+  title: 'Humanity For Prisoners',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
