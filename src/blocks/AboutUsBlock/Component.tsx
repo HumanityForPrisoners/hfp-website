a5671d402305f5c76a5b4a94955535b37d0a55c7
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-04-02 13:35:58 -0400

    About us block image styling

--- src/blocks/AboutUsBlock/Component.tsx
+++ src/blocks/AboutUsBlock/Component.tsx
@@ -9,72 +9,77 @@ import { MainGrid } from '@/components/MainGrid'
 import { PageHeading } from '@/components/PageHeading'
 import Image from 'next/image'
 import {
-  AccessibilityNew,
-  AssistWalker,
-  Hearing,
-  Info,
-  PrivacyTip,
-  Help,
-  QuestionMark,
-  Handshake,
-  VolunteerActivism,
+    AccessibilityNew,
+    AssistWalker,
+    Hearing,
+    Info,
+    PrivacyTip,
+    Help,
+    QuestionMark,
+    Handshake,
+    VolunteerActivism,
 } from '@mui/icons-material'
 
 export const AboutUsBlock: React.FC<AboutUsBlockProps> = ({ leftSide, rightSide }) => {
-  const iconMap = {
-    accessibilityNew: AccessibilityNew,
-    hearing: Hearing,
-    assistWalker: AssistWalker,
-    info: Info,
-    privacyTip: PrivacyTip,
-    help: Help,
-    questionMark: QuestionMark,
-    handshake: Handshake,
-    volunteerActivism: VolunteerActivism,
-  }
-  return (
-    <MainGrid>
-      <div className="col-span-6 flex flex-col gap-3">
-        <PageHeading {...leftSide.pageHeading} className="pb-2" />
-        <div>
-          {leftSide.values?.map(({ icon, valueTitle, valueDescription }, i) => {
-            const Icon = iconMap[icon]
-            return (
-              <div className="flex flex-row gap-4 items-center pb-4" key={i}>
-                <div className="text-6xl">
-                  <Icon className="text-secondary" fontSize="inherit" />
-                </div>
+    const iconMap = {
+        accessibilityNew: AccessibilityNew,
+        hearing: Hearing,
+        assistWalker: AssistWalker,
+        info: Info,
+        privacyTip: PrivacyTip,
+        help: Help,
+        questionMark: QuestionMark,
+        handshake: Handshake,
+        volunteerActivism: VolunteerActivism,
+    }
+    return (
+        <MainGrid>
+            <div className="col-span-6 flex flex-col gap-3">
+                <PageHeading {...leftSide.pageHeading} className="pb-2" />
+                <div>
+                    {leftSide.values?.map(({ icon, valueTitle, valueDescription }, i) => {
+                        const Icon = iconMap[icon]
+                        return (
+                            <div className="flex flex-row gap-4 items-center pb-4" key={i}>
+                                <div className="text-6xl">
+                                    <Icon className="text-secondary" fontSize="inherit" />
+                                </div>
 
-                <div className="flex flex-col gap-2">
-                  <h4>{valueTitle}</h4>
-                  <p>{valueDescription}</p>
+                                <div className="flex flex-col gap-2">
+                                    <h4>{valueTitle}</h4>
+                                    <p>{valueDescription}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
-              </div>
-            )
-          })}
-        </div>
-        <CMSLink className="w-fit" {...leftSide.ctaButton} size={'lg'} appearance={'default'} />
-      </div>
+                <CMSLink
+                    className="w-fit"
+                    {...leftSide.ctaButton}
+                    size={'lg'}
+                    appearance={'default'}
+                />
+            </div>
 
-      <div className="col-span-6 relative flex flex-col items-center">
-        <Media
-          imgClassName="rounded-[5rem] rounded-tr-none h-64 w-[25rem]"
-          className="z-11 w-fit -mb-4 pl-56"
-          resource={rightSide.topImage}
-          loading="lazy"
-        />
-        <Media
-          imgClassName="rounded-[5rem] rounded-tl-none h-64 w-[25rem]"
-          className="z-10 w-fit -mt-4 pr-56"
-          resource={rightSide.bottomImage}
-          loading="lazy"
-        />
-        <Image
-          src={Rectangle}
-          alt="Rectangle"
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[-1] object-cover"
-        />
-      </div>
-    </MainGrid>
-  )
+            <div className="col-span-6 relative flex flex-col items-center">
+                <Media
+                    imgClassName="rounded-[5rem] rounded-tr-none h-64 w-[25rem] object-cover"
+                    className="z-11 w-fit -mb-4 pl-56"
+                    resource={rightSide.topImage}
+                    loading="lazy"
+                />
+                <Media
+                    imgClassName="rounded-[5rem] rounded-tl-none h-64 w-[25rem] object-cover"
+                    className="z-10 w-fit -mt-4 pr-56"
+                    resource={rightSide.bottomImage}
+                    loading="lazy"
+                />
+                <Image
+                    src={Rectangle}
+                    alt="Rectangle"
+                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[-1] object-cover"
+                />
+            </div>
+        </MainGrid>
+    )
 }
