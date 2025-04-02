551327279d160b663429a75c5733317b08760422
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-04-02 13:30:40 -0400

    Improve readability in medium impact hero

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -5,45 +5,46 @@ import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
-  return (
-    <div
-      className="relative -mt-[7.4rem] pt-20 flex items-center justify-center text-white min-h-[80vh]"
-      data-theme="light"
-    >
-      {/* Media as Background */}
-      {media && typeof media === 'object' && (
-        <Media
-          fill
-          imgClassName="-z-10 object-cover"
-          videoClassName="-z-10 absolute inset-0 w-full h-full object-cover"
-          priority
-          resource={media}
-        />
-      )}
-      <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
-      {/* Text Content Overlay */}
-      <div className="mx-auto grid-cols-12-90 gap-30 grid pb-24 z-10">
-        <div className="md:text-left col-start-7 col-span-full">
-          {richText && (
-            <RichText
-              className="mb-6 prose-h1:text-primary 
+    return (
+        <div
+            className="relative -mt-[7.4rem] pt-20 flex items-center justify-center text-white min-h-[80vh]"
+            data-theme="light"
+        >
+            {/* Media as Background */}
+            {media && typeof media === 'object' && (
+                <Media
+                    fill
+                    imgClassName="-z-10 object-cover"
+                    videoClassName="-z-10 absolute inset-0 w-full h-full object-cover"
+                    priority
+                    resource={media}
+                    loading="eager"
+                />
+            )}
+            <div className="absolute inset-0 bg-black bg-opacity-50 z-0"></div>
+            {/* Text Content Overlay */}
+            <div className="mx-auto grid-cols-12-90 gap-30 grid pb-24 z-10">
+                <div className="md:text-left col-start-7 col-span-full">
+                    {richText && (
+                        <RichText
+                            className="mb-6 prose-h1:text-primary 
               prose-p:text-primary
               prose-h6:text-secondary drop-shadow-xl"
-              data={richText}
-              enableGutter={false}
-            />
-          )}
-          {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex gap-4">
-              {links.map(({ link }, i) => (
-                <li key={i}>
-                  <CMSLink {...link} size={'lg'} />
-                </li>
-              ))}
-            </ul>
-          )}
+                            data={richText}
+                            enableGutter={false}
+                        />
+                    )}
+                    {Array.isArray(links) && links.length > 0 && (
+                        <ul className="flex gap-4">
+                            {links.map(({ link }, i) => (
+                                <li key={i}>
+                                    <CMSLink {...link} size={'lg'} />
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
--- src/heros/MediumImpact/index.tsx
+++ src/heros/MediumImpact/index.tsx
@@ -6,31 +6,32 @@ import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
 
 export const MediumImpactHero: React.FC<Page['hero']> = ({ media, richText }) => {
-  const { headerTheme } = useHeaderTheme()
+    const { headerTheme } = useHeaderTheme()
 
-  return (
-    <div
-      className="relative -mt-[10.4rem] pt-20 flex items-center justify-center text-white"
-      data-theme={headerTheme}
-    >
-      <div className="mx-auto flex flex-col items-center justify-center pt-16 pb-16">
-        <div className="md:text-center">
-          {richText && (
-            <RichText
-              className="mb-6 prose-h2:text-primary 
+    return (
+        <div
+            className="relative -mt-[10.4rem] min-h-[60vh] pt-20 flex items-center justify-center text-white"
+            data-theme={headerTheme}
+        >
+            <div className="absolute w-full h-full mx-auto flex flex-col items-center justify-center pt-32 pb-16 z-10">
+                <div className="md:text-center h-fit">
+                    {richText && (
+                        <RichText
+                            className="prose-h2:text-primary prose-h2:drop-shadow-lg
               prose-h2:font-normal
-              prose-h6:text-secondary"
-              data={richText}
-              enableGutter={false}
-            />
-          )}
+              prose-h6:text-primary prose-h6:opacity-75"
+                            data={richText}
+                            enableGutter={false}
+                        />
+                    )}
+                </div>
+            </div>
+            <div className="select-none absolute w-full h-full">
+                {media && typeof media === 'object' && (
+                    <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
+                )}
+                <div className="absolute z-0 bg-gradient-to-t from-secondary w-full h-full" />
+            </div>
         </div>
-      </div>
-      <div className="min-h-[60vh] select-none">
-        {media && typeof media === 'object' && (
-          <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
-        )}
-      </div>
-    </div>
-  )
+    )
 }
