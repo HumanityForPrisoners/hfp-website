141e0682ae4a80265d1462a52cb885eaacca5c7f
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-04-02 12:43:40 -0400

    Organize events into past and future rows on events page

--- src/blocks/FeaturedEventsBlock/Component.tsx
+++ src/blocks/FeaturedEventsBlock/Component.tsx
@@ -15,10 +15,27 @@ export const FeaturedEventsBlock: React.FC<FeaturedEventsBlockProps> = ({
 
     return (
         <>
-            <MainGrid className="pt-8 pb-24 gap-y-28">
+            <MainGrid className="pt-8 pb-24 gap-y-14">
                 <PageHeading {...pageHeading} className="col-span-6" />
+                <h3 className="col-span-12">Upcoming Events</h3>
                 {filteredEvents.map(({ content, title, slug, eventAt }, i) => {
-                    return <EventCard key={i} reference={{ content, title, slug, eventAt }} />
+                    const today = new Date()
+                    const eventDate = new Date(eventAt as string)
+                    return (
+                        eventDate >= today && (
+                            <EventCard key={i} reference={{ content, title, slug, eventAt }} />
+                        )
+                    )
+                })}
+                <h3 className="col-span-12">Past Events</h3>
+                {filteredEvents.map(({ content, title, slug, eventAt }, i) => {
+                    const today = new Date()
+                    const eventDate = new Date(eventAt as string)
+                    return (
+                        eventDate < today && (
+                            <EventCard key={i} reference={{ content, title, slug, eventAt }} />
+                        )
+                    )
                 })}
             </MainGrid>
             {!disableCTA && (
