d0d204c220a313df64070f5ea4f4ef3ff005ffb8
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-04-06 18:37:02 -0400

    Optimize featured services block for mobile

--- src/blocks/FeaturedServicesBlock/Component.tsx
+++ src/blocks/FeaturedServicesBlock/Component.tsx
@@ -6,28 +6,28 @@ import { PageHeading } from '@/components/PageHeading'
 import { MainGrid } from '@/components/MainGrid'
 
 export const FeaturedServicesBlock: React.FC<FeaturedServicesBlockProps> = ({
-  services,
-  ctaButton,
-  pageHeading,
-  disableCTA,
+    services,
+    ctaButton,
+    pageHeading,
+    disableCTA,
 }) => {
-  const filteredServices: Service[] = (services || []).filter(
-    (s): s is Service => typeof s === 'object',
-  )
+    const filteredServices: Service[] = (services || []).filter(
+        (s): s is Service => typeof s === 'object',
+    )
 
-  return (
-    <>
-      <MainGrid className="pt-8 pb-24 gap-y-28">
-        <PageHeading {...pageHeading} className="col-span-6" />
-        {filteredServices.map(({ content, title, slug }, i) => {
-          return <ServiceCard key={i} reference={{ content, title, slug }} />
-        })}
-      </MainGrid>
-      {!disableCTA && (
-        <div className="flex w-full items-center justify-center">
-          <CMSLink {...ctaButton} appearance={'default'} size={'lg'} />
-        </div>
-      )}
-    </>
-  )
+    return (
+        <>
+            <MainGrid className="pt-8 pb-24 gap-y-28 lg:px-6">
+                <PageHeading {...pageHeading} className="col-span-6 lg:col-span-full" />
+                {filteredServices.map(({ content, title, slug }, i) => {
+                    return <ServiceCard key={i} reference={{ content, title, slug }} />
+                })}
+            </MainGrid>
+            {!disableCTA && (
+                <div className="flex w-full items-center justify-center">
+                    <CMSLink {...ctaButton} appearance={'default'} size={'lg'} />
+                </div>
+            )}
+        </>
+    )
 }
--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -4,27 +4,27 @@ import { Media } from '../Media'
 import Link from 'next/link'
 
 export type Props = {
-  reference: Pick<Service, 'slug' | 'content' | 'title'>
+    reference: Pick<Service, 'slug' | 'content' | 'title'>
 }
 
 export const ServiceCard: React.FC<Props> = (props) => {
-  const { reference } = props
-  return (
-    <div className="col-span-4 relative">
-      <Link href={`/services/${reference.slug}`}>
-        <Media
-          className="rounded-3xl h-80"
-          imgClassName="rounded-3xl w-full h-full object-cover"
-          resource={reference.content.image}
-          loading="lazy"
-        />
-        <div className="bg-secondary w-[92%] h-40 absolute z-10 -bottom-12 left-0 px-6 py-4 rounded-bl-3xl">
-          <div className="flex flex-col gap-3 text-primary">
-            <h5>{reference.title}</h5>
-            <p>{reference.content.shortDescription}</p>
-          </div>
+    const { reference } = props
+    return (
+        <div className="col-span-4 lg:col-span-full relative">
+            <Link href={`/services/${reference.slug}`}>
+                <Media
+                    className="rounded-3xl h-80"
+                    imgClassName="rounded-3xl w-full h-full object-cover"
+                    resource={reference.content.image}
+                    loading="lazy"
+                />
+                <div className="bg-secondary w-[92%] h-40 absolute z-10 -bottom-12 left-0 px-6 py-4 rounded-bl-3xl">
+                    <div className="flex flex-col gap-3 text-primary">
+                        <h5>{reference.title}</h5>
+                        <p>{reference.content.shortDescription}</p>
+                    </div>
+                </div>
+            </Link>
         </div>
-      </Link>
-    </div>
-  )
+    )
 }
