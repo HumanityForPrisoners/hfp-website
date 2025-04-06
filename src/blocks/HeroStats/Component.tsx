bb07a326ecf2bf357465559ed68fd9659c1a2c2a
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-04-06 15:52:54 -0400

    Optimize hero stats for mobile

--- src/blocks/HeroStats/Component.tsx
+++ src/blocks/HeroStats/Component.tsx
@@ -6,27 +6,28 @@ import { StatCard } from '@/components/StatCard'
 import clsx from 'clsx'
 
 export const HeroStatsBlock: React.FC<HeroStatBlockProps> = ({ Stats, options }) => {
-  return (
-    <div
-      className={clsx(
-        options.modifyForHomepage && '-mt-36',
-        'relative flex items-center justify-center',
-      )}
-    >
-      <div className="grid grid-cols-12-90 gap-30 mx-auto relative ">
-        {(Stats || []).map((stat, i) => {
-          return (
-            <StatCard
-              key={i}
-              mainStat={stat.mainStat}
-              subtitle={stat.subtitle}
-              className="col-span-3"
-              mdf={options.modifyForHomepage}
-              rmbg={options.removeBackground}
-            />
-          )
-        })}
-      </div>
-    </div>
-  )
+    return (
+        <div
+            className={clsx(
+                options.modifyForHomepage && '-mt-36',
+                'relative flex items-center justify-center',
+                options.modifyForHomepage && 'lg:hidden',
+            )}
+        >
+            <div className="grid grid-cols-12-90 gap-30 mx-auto relative">
+                {(Stats || []).map((stat, i) => {
+                    return (
+                        <StatCard
+                            key={i}
+                            mainStat={stat.mainStat}
+                            subtitle={stat.subtitle}
+                            className="col-span-3"
+                            mdf={options.modifyForHomepage}
+                            rmbg={options.removeBackground}
+                        />
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
