2d03250c3d2d902803a68cbd2116ca90f6e581dd
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-04-06 20:32:44 -0400

    Optimize featured posts for mobile

--- src/blocks/FeaturedPostsBlock/Component.tsx
+++ src/blocks/FeaturedPostsBlock/Component.tsx
@@ -6,23 +6,23 @@ import { PageHeading } from '@/components/PageHeading'
 import { MainGrid } from '@/components/MainGrid'
 
 export const FeaturedPostsBlock: React.FC<FeaturedPostsBlockProps> = ({
-  pageHeading,
-  ctaButton,
-  posts,
+    pageHeading,
+    ctaButton,
+    posts,
 }) => {
-  const filteredPosts: Post[] = (posts || []).filter((s): s is Post => typeof s === 'object')
+    const filteredPosts: Post[] = (posts || []).filter((s): s is Post => typeof s === 'object')
 
-  return (
-    <div className="pt-8 flex flex-col h-auto gap-1">
-      <MainGrid className="h-fit">
-        <PageHeading {...pageHeading} className="col-span-6" />
-        {filteredPosts.map(({ content, title, slug }, i) => {
-          return <PostCard key={i} reference={{ content, title, slug }} />
-        })}
-      </MainGrid>
-      <div className="flex w-full items-center justify-center">
-        <CMSLink {...ctaButton} appearance={'default'} size={'lg'} />
-      </div>
-    </div>
-  )
+    return (
+        <div className="pt-8 flex flex-col h-auto gap-1">
+            <MainGrid className="h-fit lg:px-6">
+                <PageHeading {...pageHeading} className="col-span-6 lg:col-span-full" />
+                {filteredPosts.map(({ content, title, slug }, i) => {
+                    return <PostCard key={i} reference={{ content, title, slug }} />
+                })}
+            </MainGrid>
+            <div className="flex w-full items-center justify-center">
+                <CMSLink {...ctaButton} appearance={'default'} size={'lg'} />
+            </div>
+        </div>
+    )
 }
