1095f90c3dc2d3b37dfd4f240ab574548d404aae
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-04-06 20:55:14 -0400

    Optimize team members for mobile

--- src/blocks/TeamMembersBlock/Component.tsx
+++ src/blocks/TeamMembersBlock/Component.tsx
@@ -6,26 +6,32 @@ import { PageHeading } from '@/components/PageHeading'
 import { MainGrid } from '@/components/MainGrid'
 
 export const TeamMembersBlock: React.FC<TeamMembersBlockProps> = ({
-  teams,
-  ctaButton,
-  pageHeading,
-  disableCTA,
+    teams,
+    ctaButton,
+    pageHeading,
+    disableCTA,
 }) => {
-  const filteredTeams: Team[] = (teams || []).filter((t): t is Team => typeof t === 'object')
+    const filteredTeams: Team[] = (teams || []).filter((t): t is Team => typeof t === 'object')
 
-  return (
-    <>
-      <MainGrid className="pt-8 pb-16 gap-y-16">
-        <PageHeading {...pageHeading} className="col-span-6" />
-        {filteredTeams.map((team, i) => {
-          return <TeamCard className="col-span-4" key={i} reference={team} />
-        })}
-      </MainGrid>
-      {!disableCTA && (
-        <div className="flex w-full pb-8 items-center justify-center">
-          <CMSLink {...ctaButton} appearance={'default'} size={'lg'} />
-        </div>
-      )}
-    </>
-  )
+    return (
+        <>
+            <MainGrid className="pt-8 pb-16 gap-y-16 lg:px-6">
+                <PageHeading {...pageHeading} className="col-span-6 lg:col-span-full" />
+                {filteredTeams.map((team, i) => {
+                    return (
+                        <TeamCard
+                            className="col-span-4 lg:col-span-full"
+                            key={i}
+                            reference={team}
+                        />
+                    )
+                })}
+            </MainGrid>
+            {!disableCTA && (
+                <div className="flex w-full pb-8 items-center justify-center">
+                    <CMSLink {...ctaButton} appearance={'default'} size={'lg'} />
+                </div>
+            )}
+        </>
+    )
 }
