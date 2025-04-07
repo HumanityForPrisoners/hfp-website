05f777ea0a6966da964293caabebf3b5af679b5d
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-04-06 21:14:34 -0400

    Optimize services page for mobile

--- src/app/(frontend)/services/[slug]/page.tsx
+++ src/app/(frontend)/services/[slug]/page.tsx
@@ -12,87 +12,87 @@ import PageClient from './page.client'
 import { Media } from '@/components/Media'
 
 export async function generateStaticParams() {
-  const payload = await getPayload({ config: configPromise })
-  const services = await payload.find({
-    collection: 'services',
-    limit: 1000,
-    overrideAccess: false,
-    pagination: false,
-    select: {
-      slug: true,
-    },
-  })
+    const payload = await getPayload({ config: configPromise })
+    const services = await payload.find({
+        collection: 'services',
+        limit: 1000,
+        overrideAccess: false,
+        pagination: false,
+        select: {
+            slug: true,
+        },
+    })
 
-  const params = services.docs.map(({ slug }) => {
-    return { slug }
-  })
+    const params = services.docs.map(({ slug }) => {
+        return { slug }
+    })
 
-  return params
+    return params
 }
 
 type Args = {
-  params: Promise<{
-    slug?: string
-  }>
+    params: Promise<{
+        slug?: string
+    }>
 }
 
 export default async function Service({ params: paramsPromise }: Args) {
-  const { slug = '' } = await paramsPromise
-  const url = '/services/' + slug
-  const service = await queryServiceBySlug({ slug })
+    const { slug = '' } = await paramsPromise
+    const url = '/services/' + slug
+    const service = await queryServiceBySlug({ slug })
 
-  if (!service) return <PayloadRedirects url={url} />
+    if (!service) return <PayloadRedirects url={url} />
 
-  return (
-    <article className="pt-16 pb-16">
-      <PageClient />
+    return (
+        <article className="pt-16 pb-16">
+            <PageClient />
 
-      {/* Allows redirects for valid pages too */}
-      <PayloadRedirects disableNotFound url={url} />
-      <RenderHero {...service.hero.hero} />
-      <div className="flex items-center justify-center">
-        <div className="mx-auto grid-cols-12-90 gap-30 grid pt-16 pb-24">
-          <div className="col-span-12 relative">
-            <Media
-              imgClassName="rounded-3xl w-full max-h-[90vh] object-cover"
-              resource={service.content.image}
-            />
-            <div className="bg-secondary w-fit h-fit absolute z-10 -bottom-12 left-0 px-12 py-8 rounded-bl-3xl">
-              <h3 className="text-primary">{service.title}</h3>
+            {/* Allows redirects for valid pages too */}
+            <PayloadRedirects disableNotFound url={url} />
+            <RenderHero {...service.hero.hero} />
+            <div className="flex items-center justify-center">
+                <div className="mx-auto grid-cols-12-90 gap-30 grid pt-16 pb-24 lg:px-6">
+                    <div className="col-span-12 relative">
+                        <Media
+                            imgClassName="rounded-3xl w-full max-h-[90vh] object-cover"
+                            resource={service.content.image}
+                        />
+                        <div className="bg-secondary w-fit h-fit absolute z-10 -bottom-12 left-0 px-12 py-8 rounded-bl-3xl">
+                            <h3 className="text-primary">{service.title}</h3>
+                        </div>
+                    </div>
+
+                    <RichText
+                        className="col-span-12 mt-20 prose-h5:text-secondary"
+                        data={service.content.description}
+                        enableGutter={false}
+                    />
+                </div>
             </div>
-          </div>
-
-          <RichText
-            className="col-span-12 mt-20 prose-h5:text-secondary"
-            data={service.content.description}
-            enableGutter={false}
-          />
-        </div>
-      </div>
-    </article>
-  )
+        </article>
+    )
 }
 
 export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
-  const { slug = '' } = await paramsPromise
-  const service = await queryServiceBySlug({ slug })
+    const { slug = '' } = await paramsPromise
+    const service = await queryServiceBySlug({ slug })
 
-  return generateMeta({ doc: service })
+    return generateMeta({ doc: service })
 }
 
 const queryServiceBySlug = cache(async ({ slug }: { slug: string }) => {
-  const payload = await getPayload({ config: configPromise })
+    const payload = await getPayload({ config: configPromise })
 
-  const result = await payload.find({
-    collection: 'services',
-    limit: 1,
-    pagination: false,
-    where: {
-      slug: {
-        equals: slug,
-      },
-    },
-  })
+    const result = await payload.find({
+        collection: 'services',
+        limit: 1,
+        pagination: false,
+        where: {
+            slug: {
+                equals: slug,
+            },
+        },
+    })
 
-  return result.docs?.[0] || null
+    return result.docs?.[0] || null
 })
