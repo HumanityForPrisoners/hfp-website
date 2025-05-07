c3daf94ff29260a2f1e6778ee3afe28333bf703d
vincenzomeschi <meschiv@mail.gvsu.edu>
2025-05-07 11:39:25 -0400

    Remove vercel image optimization

--- src/components/Media/VideoMedia/index.tsx
+++ src/components/Media/VideoMedia/index.tsx
@@ -8,39 +8,39 @@ import type { Props as MediaProps } from '../types'
 import { getClientSideURL } from '@/utilities/getURL'
 
 export const VideoMedia: React.FC<MediaProps> = (props) => {
-  const { onClick, resource, videoClassName } = props
+    const { onClick, resource, videoClassName } = props
 
-  const videoRef = useRef<HTMLVideoElement>(null)
-  // const [showFallback] = useState<boolean>()
+    const videoRef = useRef<HTMLVideoElement>(null)
+    // const [showFallback] = useState<boolean>()
 
-  useEffect(() => {
-    const { current: video } = videoRef
-    if (video) {
-      video.addEventListener('suspend', () => {
-        // setShowFallback(true);
-        // console.warn('Video was suspended, rendering fallback image.')
-      })
+    useEffect(() => {
+        const { current: video } = videoRef
+        if (video) {
+            video.addEventListener('suspend', () => {
+                // setShowFallback(true);
+                // console.warn('Video was suspended, rendering fallback image.')
+            })
+        }
+    }, [])
+
+    if (resource && typeof resource === 'object') {
+        const { filename } = resource
+
+        return (
+            <video
+                autoPlay
+                className={cn(videoClassName)}
+                controls={false}
+                loop
+                muted
+                onClick={onClick}
+                playsInline
+                ref={videoRef}
+            >
+                <source src={`${getClientSideURL()}/api/media/file/${filename}`} />
+            </video>
+        )
     }
-  }, [])
 
-  if (resource && typeof resource === 'object') {
-    const { filename } = resource
-
-    return (
-      <video
-        autoPlay
-        className={cn(videoClassName)}
-        controls={false}
-        loop
-        muted
-        onClick={onClick}
-        playsInline
-        ref={videoRef}
-      >
-        <source src={`${getClientSideURL()}/api/media/file/${filename}`} />
-      </video>
-    )
-  }
-
-  return null
+    return null
 }
